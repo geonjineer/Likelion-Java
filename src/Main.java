d1f622c61f87ed9de69f54b2b4359f956a3d2860
geonjineer <atoo1520@gmail.com>
2025-05-30 14:59:13 +0900

    commit-variable casting

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,14 @@ public class Main {
         
         double salary = 50000.75;
         System.out.println("salary = " + salary);
-
+        int bonus = 500;
+        
         String name = "정건진";
         System.out.println("name = " + name);
         System.out.println(name.length());
+        
+        int totalsalary = (int) salary + bonus;
+        System.out.println("totalsalary = " + totalsalary);
 
     }
 }

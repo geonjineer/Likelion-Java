3eafe8bfee7004bd9ee8c1a33ffd214d1e4c0038
geonjineer <atoo1520@gmail.com>
2025-05-30 14:35:39 +0900

    commit-variables

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
         int age = 25;
         System.out.println("age = " + age);
         
+        double salary = 50000.75;
+        System.out.println("salary = " + salary);
+
+        String name = "정건진";
+        System.out.println("name = " + name);
+        System.out.println(name.length());
 
     }
 }
